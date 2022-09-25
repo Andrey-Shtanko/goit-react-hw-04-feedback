9b0cd3396bea81e2479d93fa5f7467f3dd47cbae
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-09-25 13:15:17 +0300

    add-buttons-functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,23 @@ import { Statictics } from './Statistics/Statistics';
 
 export class App extends Component {
   state = {
-    good: 10,
-    neutral: 20,
-    bad: 30,
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = feedback => {
+    this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
   };
 
   render() {
     return (
       <Container>
         <Title>Please leave feedback</Title>
-        <Buttons feedbacks={Object.keys(this.state)}></Buttons>
+        <Buttons
+          feedbacks={Object.keys(this.state)}
+          onHandleIncrement={this.handleIncrement}
+        ></Buttons>
         <Statictics
           feedbacks={Object.keys(this.state)}
           stats={this.state}
--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,11 +1,13 @@
 import { ButtonsList } from './Buttons.styled';
 
-export const Buttons = ({ feedbacks }) => {
+export const Buttons = ({ feedbacks, onHandleIncrement }) => {
   return (
     <ButtonsList>
-      {feedbacks.map(button => (
-        <li key={button}>
-          <button>{button}</button>
+      {feedbacks.map(feedback => (
+        <li key={feedback}>
+          <button onClick={() => onHandleIncrement(feedback)}>
+            {feedback}
+          </button>
         </li>
       ))}
     </ButtonsList>
